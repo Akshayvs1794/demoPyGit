505d080cb710e1e20b03ab2d517924f0192a627f
Akshay <akshayvs1794@gmail.com>
2024-09-25 19:52:32 +0530

    some lines removed ..

--- demo.py
+++ demo.py
@@ -1,7 +1,3 @@
 print("Hello Akshay")
 print("Hello from my pyCharm  ..")
 print("25 September 2024 ..")
-print("Hello Sayali, Love you ..")
-print("Hello Sayali, Love you ..")
-print("Hello Sayali, Love you ..")
-print("Hello Sayali, Love you ..")
